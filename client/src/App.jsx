bd2b0c0be56127d3fc19d8ffac88625277178bc1
Archit Sehgal <architsehgal19@gmail.com>
2023-10-20 01:36:34 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+
+function App() {
+  const [tax, setTax] = useState("");
+  const [income, setincome] = useState("");
+  const getTaxValue = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://127.0.0.1:8080/calculate/tax", {income })
+      .then((response) => {
+        setTax(response.data.taxvalue);
+        console.log(response)
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  };
+
+  return (
+    <div className="main flex">
+      <h1>get value of tax</h1>
+      <label>enter the income before tax:</label>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined"
+        type="number"
+        value={income}
+        onChange={(e) => {
+          setincome(e.target.value);
+        }}
+      />
+      <Button variant="contained" onClick={getTaxValue}>get tax value</Button>
+      {tax &&(
+      <Alert severity="info">Tax payable: <b style={{letterSpacing:".1rem"}}>{tax}</b></Alert>)}
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def home():
+    return "hey there"
+@app.route("/calculate/tax", methods=["POST"])
+def calculatetax():
+    try:
+        data = request.get_json()
+        
+        income = int(data["income"])
+        
+        if income <= 300000:
+            tax = 0
+        elif income <= 600000:
+            tax = (300000 / 100) * 5
+        elif income <= 900000:
+            tax = (300000 / 100) * 5 + ((income - 600000) / 100) * 10
+        elif income <= 1200000:
+            tax = (300000 / 100) * 5 + (300000 / 100) * 10 + ((income - 900000) / 100) * 15
+        elif income <= 1500000:
+            tax = (300000 / 100) * 5 + (300000 / 100) * 10 + (300000 / 100) * 15 + ((income - 1200000) / 100) * 20
+        else:
+            tax = (300000 / 100) * 5 + (300000 / 100) * 10 + (300000 / 100) * 15 + (300000 / 100) * 20 + ((income - 1500000) / 100) * 30
+
+        cess = (tax / 100) * 4
+        taxvalue = tax + cess
+
+        formatted_taxvalue = "{:.2f}".format(taxvalue)
+
+        return jsonify({"taxvalue": formatted_taxvalue})
+    
+    except Exception as e:
+        return jsonify({"error": str(e)})
+if __name__ == "__main__":
+    app.run(port=8080)
